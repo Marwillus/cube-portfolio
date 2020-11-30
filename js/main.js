51e3ac0aabe407ed1962df74dac4ad0432d38399
marwillus <marcusxwill@gmail.com>
2020-11-30 21:44:27 +0100

    done

--- js/main.js
+++ js/main.js
@@ -1,20 +1,5 @@
-var swiperH = new Swiper(".swiper-container-h", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  speed: 800,
-  initialSlide: 1,
-  // effect: "cube",
-  // effect: "flip",
-  /*   cubeEffect: {
-    shadow: false,
-    slideShadows: true,
-    shadowOffset: 20,
-    shadowScale: 0.94,
-  }, */
-});
 var swiperV = new Swiper(".swiper-container-v", {
+  initialSlide: 2,
   direction: "vertical",
   mousewheel: true,
   nested: true,
@@ -35,5 +20,14 @@ var swiperV = new Swiper(".swiper-container-v", {
       var names = ["home", "about", "projects", "contact"];
       return '<span class="' + className + '">' + names[index] + "</span>";
     },
+  },
+});
+
+var swiper = new Swiper(".swiper-container-h", {
+  initialSlide: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    navigationHide: true,
   },
 });
